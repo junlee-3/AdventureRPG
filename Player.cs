4a0393ccf3c60a347dc0121730311242e0600b80
Jun3-dev <jun.lee73@outlook.com>
2023-03-03 09:54:14 +1100

    Created player class, and asked the player of what class of player they would like to be

---
+++ Player.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventureRPG;
+
+public class Player
+{
+    public static string? Name;
+    public static int Health;
+    public static string[]? Inventory;
+    public static string? PlayerClass;
+    
+    //Constructor
+    public Player(string? name, int health, string[]? inventory)
+    {
+        Name = name;
+        Health = health;
+        Inventory = inventory;
+    }
+
+    public class Warrior
+    {
+        public static string[] Attacks = {"Sword Slash", "Shield Bash", "Heavy Swing", "Punch"};
+    }
+
+    public class Mage
+    {
+        public static string[] Attacks = { "Fireball", "Ice Shard", "Lightning Bolt", "Heal" };
+    }
+
+    public class Archer
+    {
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace AdventureRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Welcome to the Adventure RPG!");
+
+            Console.WriteLine("What is your name?");
+            Player.Name = Console.ReadLine();
+            Player.Health = 100;
+            Player.Inventory = new string[10];
+            
+            Console.WriteLine($"Welcome! {Player.Name}");
+            Console.WriteLine("Choose Your Class of Player! (Warrior, Mage, or Archer)");
+            Player.PlayerClass = Console.ReadLine();
+
+            switch (Player.PlayerClass)
+            {
+                case "Mage":
+                    Console.WriteLine("You are a Mage! Your Attacks are:");
+                    foreach (var attack in Player.Mage.Attacks)
+                    {
+                        Console.WriteLine(attack);
+                    }
+                    break;
+                
+                case "Warrior":
+                    Console.WriteLine("You are a Warrior!");
+                    foreach (var attack in Player.Warrior.Attacks)
+                    {
+                        Console.WriteLine(attack);
+                    }
+                    
+                    break;
+                case "Archer":
+                    Console.WriteLine("You are an Archer!");
+                    break;
+                default:
+                    Console.WriteLine("Please choose a valid class of player, use the correct spelling!");
+                    break;
+            }
+            
+            
+        } 
     }
 }
