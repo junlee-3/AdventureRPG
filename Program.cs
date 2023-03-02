455c091e2b0a950052e512c9d1dff74878673a27
Jun3-dev <jun.lee73@outlook.com>
2023-03-03 09:21:06 +1100

    Code Setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace AdventureRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
